201c99e73cd33ad12d6d38264a72e0e2eb18c6b4
biancavaz <147559714+biancavaz@users.noreply.github.com>
2025-03-04 12:11:46 -0300

    construido a controller do editor

--- hav-imobiliaria/src/main/java/com/hav/hav_imobiliaria/controller/AdmController.java
+++ hav-imobiliaria/src/main/java/com/hav/hav_imobiliaria/controller/AdmController.java
@@ -1,4 +1,16 @@
 package com.hav.hav_imobiliaria.controller;
 
+import com.hav.hav_imobiliaria.service.AdmService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/adm")
+@AllArgsConstructor
 public class AdmController {
+
+    private AdmService service;
+
+
 }
--- hav-imobiliaria/src/main/java/com/hav/hav_imobiliaria/service/AdmService.java
+++ hav-imobiliaria/src/main/java/com/hav/hav_imobiliaria/service/AdmService.java
@@ -1,4 +1,10 @@
 package com.hav.hav_imobiliaria.service;
 
+import com.hav.hav_imobiliaria.repository.AdmRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AdmService {
+
+    private AdmRepository repository;
 }
