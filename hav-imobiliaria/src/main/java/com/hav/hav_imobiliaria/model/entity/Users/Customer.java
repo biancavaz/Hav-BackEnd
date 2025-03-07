20b7d3ed10ea3819abad01e259790b1a9349ad8b
biancavaz <147559714+biancavaz@users.noreply.github.com>
2025-03-07 13:54:36 -0300

    alteração do nome

--- hav-imobiliaria/src/main/java/com/hav/hav_imobiliaria/model/entity/Users/Customer.java
+++ hav-imobiliaria/src/main/java/com/hav/hav_imobiliaria/model/entity/Users/Customer.java
@@ -11,7 +11,7 @@ import lombok.experimental.SuperBuilder;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "customer_owner")
+@Table(name = "customer")
 @SuperBuilder
 public class Customer extends User {
 
