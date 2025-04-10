316e116c96bede34fe3dcc95e3701cc35ed8cf57
RafaelHR12 <147559750+RafaelHR12@users.noreply.github.com>
2025-04-09 21:11:42 -0300

    Configuração WebSocket

---
+++ hav-imobiliaria/src/main/java/com/hav/hav_imobiliaria/security/controllerSecurity/RealtimeChat.java
@@ -0,0 +1,28 @@
+package com.hav.hav_imobiliaria.security.controllerSecurity;
+
+import com.hav.hav_imobiliaria.security.modelSecurity.Message;
+import lombok.AllArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@AllArgsConstructor
+public class RealtimeChat {
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    @MessageMapping("/message")
+    @SendTo("/group/public")
+    public Message receiveMessage(@Payload Message message) {
+
+        messagingTemplate.convertAndSend(
+                "/group/" + message.getChat().getId().toString(), message);
+
+        return message;
+    }
+}
