11606f5a28ca8163030846df6788088d8e2253d8
biancavaz <147559714+biancavaz@users.noreply.github.com>
2025-04-22 21:56:17 -0300

    arrumando maps

--- hav-imobiliaria/src/main/java/com/hav/hav_imobiliaria/security/configSecurity/TokenProvider.java
+++ hav-imobiliaria/src/main/java/com/hav/hav_imobiliaria/security/configSecurity/TokenProvider.java
@@ -4,8 +4,12 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import lombok.AllArgsConstructor;
+<<<<<<< Updated upstream
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+=======
+//import org.springframework.security.core.Authentication;
+>>>>>>> Stashed changes
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
