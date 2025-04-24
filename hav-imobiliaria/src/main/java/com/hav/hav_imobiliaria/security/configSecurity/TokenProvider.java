21cb9d209df5da0dcf069421c92cd4731bfe2748
biancavaz <147559714+biancavaz@users.noreply.github.com>
2025-04-24 17:54:42 -0300

    arrumando dev

--- hav-imobiliaria/src/main/java/com/hav/hav_imobiliaria/security/configSecurity/TokenProvider.java
+++ hav-imobiliaria/src/main/java/com/hav/hav_imobiliaria/security/configSecurity/TokenProvider.java
@@ -3,13 +3,8 @@ package com.hav.hav_imobiliaria.security.configSecurity;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
-import lombok.AllArgsConstructor;
-<<<<<<< Updated upstream
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-=======
-//import org.springframework.security.core.Authentication;
->>>>>>> Stashed changes
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
